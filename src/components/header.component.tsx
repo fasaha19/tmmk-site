8bb790e1597539143ae04641c4e96acd4f6f5406
fasaha19 <fasaha.techy@gmail.com>
2023-03-19 23:00:10 +0530

    changes

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -79,20 +79,11 @@ export const Header = () => {
               </svg>
             </a>
             <button className="navbar-close" onClick={toggleNavBar}>
-              <svg
-                className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
+              <img
+                src="https://members.tmmk.info/assets/images/flag_tmmk.jpg"
+                alt=""
+                className="h-12 w-[4rem]"
+              />
             </button>
           </div>
           <div>
@@ -139,7 +130,7 @@ export const Header = () => {
               </li>
             </ul>
           </div>
-          <div className="mt-auto">
+          {/* <div className="mt-auto">
             <div className="pt-6">
               <a
                 className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl"
@@ -157,7 +148,7 @@ export const Header = () => {
             <p className="my-4 text-xs text-center text-gray-400">
               <span>Copyright © 2021</span>
             </p>
-          </div>
+          </div> */}
         </nav>
       </div>
     </>
